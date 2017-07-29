e612870fdb2eff78dbb60d94317f73954ee496e1
heaven231 <285472475@qq.com>
2017-07-29 10:25:05 +0800

    提交代码

--- Android.mk
+++ Android.mk
@@ -12,7 +12,6 @@ LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 LOCAL_PACKAGE_NAME := PackageInstaller
 LOCAL_CERTIFICATE := platform
 
-########################
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
